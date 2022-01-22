6209e52483b060acae0aba5a7f1dc0a7fe93ae21
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-01-21 23:10:54 -0300

    estudando axios e async await

--- EstyudoASyncAwait.js
+++ EstyudoASyncAwait.js
@@ -20,3 +20,22 @@ axios.get(`https://swapi.dev/api/people/2`)
     .then(filme => filme.planets[2])
     .then(planetaUrl => axios.get(planetaUrl))
     .then(resp => console.log(resp.data))
+
+// const axios = require('axios')
+    
+// async function getWhatToDo(id){
+//     const resp = await axios.get(`http://www.boredapi.com/api/activity?key=${id}`)
+//     return resp.data
+// }
+
+// async function getWhatToDoBy(type){
+//     const resp = await axios.get(`http://www.boredapi.com/api/activity?type=${type}`)
+//     return resp.data
+// }
+
+// (async function(){
+//     const thingsToDo = await getWhatToDo('3943506')
+//     console.log(thingsToDo)
+//     const type = await getWhatToDoBy(thingsToDo.type)
+//     console.log(type)
+// })()
